a10443d5ef7f5416ff916f347101217eed7ea1fe
vishnuteja452 <vishnu223w@gmail.com>
2026-06-13 19:31:30 +0530

    Fix statuscode casing reference inside Express error middleware to prevent response crashes

--- vidtube/src/middlewares/error.middleware.js
+++ vidtube/src/middlewares/error.middleware.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utils/apierror.js";
 const errorhandler = (err,req,res,next) =>{
     let error = err
     if(!(error instanceof ApiError)){
-        const statuscode = error.statuscode || error instanceof mongoose.Error ? 400 : 500
+        const statuscode = error.statusCode || error.statuscode || (error instanceof mongoose.Error ? 400 : 500)
         const message = error.message || "something went wrong"
         error = new ApiError(statuscode,message,error?.errors || [],err.stack)
     }
@@ -13,7 +13,8 @@ const errorhandler = (err,req,res,next) =>{
         message:error.message,
         ...(process.env.NODE_ENV === "development" && {stack:error.stack})
     }
-    return res.status(error.statusCode).json(response)
+    const finalStatusCode = error.statusCode || error.statuscode || 500;
+    return res.status(finalStatusCode).json(response)
 }
 
 export default errorhandler;
